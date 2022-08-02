9e86f9114bcd5eaf9ebe28885f00323e2dd07803
Omaru Jawara <omarito.jawara@gmail.com>
2022-08-02 17:38:58 -0400

    frequency

--- Electricity/electricity.pl
+++ Electricity/electricity.pl
@@ -3,14 +3,17 @@
  use feature "say";
  use warnings;
 
+my %words_freq = ();
 my @records; 
-open(FILE_WRITE,">sortAlphabeticallyIngoreCase.txt");
-open(FILE_READ,"<electricity.txt"); # read from electricity file
+open(FILE_WRITE,">./Electricity/electricity_output.txt");
+open(FILE_READ,"<./Electricity/electricity.txt") or die $!; # read from electricity file
  while(<FILE_READ>) {
-    $_ =~ s/(\.+|\,+|\[|\]|\"|\(|\)|\:|\-|[0-9])//g;
+    $_ =~ s/(\.+|\,+|\[|\]|\"|\(|\)|\:|\-|[0-9])//g; #should I reomve apostrophe
     chomp;
-    foreach my $arr (split /[\s+]/ , $_) {
-        push @records, $arr ;
+    foreach my $word (split /[\s+]/ , $_) {
+        $word =~ tr/A-Z/a-z/;
+        push @records, $word;
+        ++@words_freq{$word};
     }
  }
 close(FILE_READ);
@@ -19,4 +22,11 @@ foreach my $word (sort { $a cmp $b} @records) {
     if ($word eq "") { next; }
     else { say FILE_WRITE $word; }
 }
-close(FILE_WRITE)
+close(FILE_WRITE);
+
+while (( my $key, my $value) = each(%words_freq))
+{
+      
+    # Printing(key, value) pair
+    print("$key => $value\n");
+}
